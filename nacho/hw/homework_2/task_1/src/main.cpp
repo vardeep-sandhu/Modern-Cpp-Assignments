3b7b00c033de9cd608f13ab0b5731ade9e136712
vardeep-sandhu <good.coding.is.that.you@gmail.com>
2023-05-02 12:58:53 +0200

    added hw2.1 commit

--- nacho/hw/homework_2/task_1/src/main.cpp
+++ nacho/hw/homework_2/task_1/src/main.cpp
@@ -10,14 +10,34 @@ int main() {
 
   std::cout << "The random generated number is: " << random_number << std::endl;
   std::cout << "Now please enter a number: " << std::endl;
-
+  std::string entry{};
   int number = 0;
-  std::cin >> number;
-  std::cout << "Did you enter: " << number << std::endl;
 
-  if (random_number == number) {
-    std::cout << "Exiting now!" << std::endl;
-    return EXIT_SUCCESS;
+  while (true) {
+
+    std::cin >> entry;
+    number = std::stoi(entry);
+    std::cout << "Did you enter: " << number << std::endl;
+
+    if (random_number == number) {
+      std::cout << "You have won" << std::endl;
+      return EXIT_SUCCESS;
+    }
+
+    else if (random_number > number) {
+      std::cout << "My number is larger, guess again" << std::endl;
+      continue;
+    }
+
+    else if (random_number < number) {
+      std::cout << "My number is smaller, guess again" << std::endl;
+      continue;
+    }
+
+    else {
+      std::cerr << "Something weird happened!" << std::endl;
+      return EXIT_FAILURE;
+    }
   }
   return 0;
 }
